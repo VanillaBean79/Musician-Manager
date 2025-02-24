1fab95a021ccd18e46607f8f5d85a53d6116de54
Ruben Salazar <rubensalazar@rocketmail.com>
2025-02-24 11:50:29 -0600

    class Manager created with dictionary and init

--- lib/models/Manager.py
+++ lib/models/Manager.py
@@ -1,2 +1,34 @@
 import sqlite3
 
+CONN = sqlite3.connect('musicians.db')
+CURSOR = CONN.cursor()
+
+#Create the tables
+def create_tables():
+    with CONN:
+        CONN.execute("""
+            CREATE TABLE IF NOT EXIST managers (
+                     id INTEGER PRIMARY KEY,
+                     name TEXT NOT NULL,
+                     age INTEGER NOT NULL )
+                     """)
+            
+        CONN.execute("""
+            CREATE TABLE IF NOT EXIST musicians (
+                     id INTEGER PRIMARY KEY,
+                     name TEXT NOT NULL,
+                     age INTEGER NOT NULL,
+                     instrument TEXT NOT NULL,
+                     category TEXT NOT NULL,
+                     manager_id INTEGER,
+                     FOREIGN KEY(manager_id) REFERENCES manager(id)
+                     )
+                     """)
+
+class Manager:
+    all = {} # A dictionary of manager data
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Manager.all[name] = self
